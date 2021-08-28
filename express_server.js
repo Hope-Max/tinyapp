ecda16c9417e8f6d8285e2193e7ce690522c7be0
Max Li <hope_max@outlook.com>
2021-08-28 16:32:13 -0400

    Refactored

--- express_server.js
+++ express_server.js
@@ -33,13 +33,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello!');// res.write() && res.send()
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello!');// res.write() && res.send()
+// });
 
 // Add an additional endpoints
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);// JSON.stringify() && res.send()
+// app.get('/urls.json', (req, res) => {
+//   res.json(urlDatabase);// JSON.stringify() && res.send()
+// });
+
+// Add an additional endpoints with the response that contain HTML code
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 // Add an additional endpoints
@@ -48,14 +53,17 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars); // use res.render() to pass/send the data to template
 });
 
+app.post('/urls', (req, res) => {
+  const shortURL = generateRandomString(6);
+  urlDatabase[shortURL] = `http://${req.body.longURL}`;
+  // console.log(req.body);  // Log the POST request body to the console
+  // res.send('Ok');         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortURL}`);
+});
+
 // Add additional endpoints, this route handler will render the page with the form
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 // Add an additional endpoints
@@ -64,10 +72,7 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
-// Add an additional endpoints with the response that contain HTML code
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
