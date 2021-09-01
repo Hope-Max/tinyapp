400349816a5e8789a7ede0fd89d60f611be51c1a
Max Li <hope_max@outlook.com>
2021-09-01 15:26:40 -0400

    Create a Registration Handler

--- express_server.js
+++ express_server.js
@@ -32,6 +32,18 @@ const generateRandomString = function(n) {
 app.set('view engine', 'ejs');
 
 const urlDatabase = {};
+const users = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
 
 // app.get('/', (req, res) => {
 //   res.send('Hello!');// res.write() && res.send()
@@ -49,7 +61,8 @@ app.get('/hello', (req, res) => {
 
 // Read GET /urls
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
+  const id = req.cookies.user_id;
+  const templateVars = { urls: urlDatabase, user: users[id] };
   res.render('urls_index', templateVars); // use res.render() to pass/send the data to template
 });
 
@@ -64,13 +77,16 @@ app.post('/urls', (req, res) => {
 
 // Add additional endpoints, this route handler will render the page with the form
 app.get('/urls/new', (req, res) => {
-  const templateVars = { username: req.cookies['username']};
+  const id = req.cookies.user_id;
+  const templateVars = { user: users[id] };
   res.render('urls_new', templateVars);
 });
 
 // Read GET /urls/:shortURL
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies['username'] };
+  const id = req.cookies.user_id;
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[id] };
+
   res.render('urls_show', templateVars);
 });
 
@@ -82,6 +98,7 @@ app.get("/u/:shortURL", (req, res) => {
     return res.end('404 Page Not Found\n');
   }
   const longURL = urlDatabase[shortURL];
+
   res.redirect(longURL);
 });
 
@@ -89,6 +106,7 @@ app.get("/u/:shortURL", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
+
   res.redirect('/urls');
 });
 
@@ -96,25 +114,40 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = `http://${req.body.longURL}`;
+
   res.redirect(`/urls/${shortURL}`);
 });
 
 // Add POST /login
 app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);// set a cookie
+  const id = req.cookies.user_id;
+  res.cookie('user_id', id);// set a cookie
+
   res.redirect("/urls");
 });
 
 // Add POST /logout
 app.post("/logout", (req, res) => {
-  res.clearCookie('username'); // clears the username cookie
+  res.clearCookie('user_id'); // clears the username cookie
+
   res.redirect('urls');
 });
 
 // Read GET /register
 app.get("/register", (req, res) => {
-  const templateVars = { username: req.cookies['username']};
-  res.render('urls_registration', templateVars);
+  res.render('urls_registration');
+});
+
+// Add POST /register
+app.post("/register", (req, res) => {
+  const id = generateRandomString(8);
+  const email = req.body.email;
+  const password = req.body.password;
+  users[id] = { id, email, password };
+
+  res.cookie('user_id', id);
+
+  res.redirect('/urls');
 });
 
 app.listen(port, () => {
