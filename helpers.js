3e62178c0bef39ccf56bf6968eb516d4b14cd068
Max Li <hope_max@outlook.com>
2021-09-09 20:17:46 -0400

    Refactor the generateRandomString function by manipulating string instead of array

--- helpers.js
+++ helpers.js
@@ -20,20 +20,13 @@ const urlsForUser = function(personalUserID, urlDatabase) {
   return personalUrlDatabase;
 };
 
-// Generates a random 6-digit string consisting of numbers and lowercase letters
-// const generateRandomString1 = function() {
-//   return Math.random().toString(36).substr(2, 6);
-// };
-
 // Generates a random n-digit string consisting of numbers and uppercase or lowercase letters
 const generateRandomString = function(n) {
-  const arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  const letters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let str = '';
 
   for (let i = 0; i < n; i++) {
-    const randomChar = arr[Math.round(Math.random() * (arr.length - 1))];
+    const randomChar = letters[Math.round(Math.random() * (letters.length - 1))];
     str += randomChar;
   }
   return str;
